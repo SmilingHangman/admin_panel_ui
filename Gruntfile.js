a50f2fdfd858f825f2d603f25b4e2ab7525a3add
SmilingHangman <andrejus.chomicenka@gmail.com>
2019-09-16 21:37:15 +0300

    nav_menu

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,49 +1,29 @@
-module.exports = function (grunt) {
-    grunt.initConfig({
-      compass: {                  // Task
-        dist: {                   // Target
-          options: {              // Target options
-            sassDir: 'src/assets/style',
-            cssDir: 'assets/style',
-            environment: 'production'
-          }
-        },
+module.exports = function(grunt) {
+  grunt.initConfig({
+    compass: {                  // Task
+      dist: {                   // Target
+        options: {              // Target options
+          sassDir: 'src/assets/style',
+          cssDir: 'public/assets/style',
+          environment: 'production'
+        }
       },
-      watch: {
+    },
+    watch: {
+      css: {
+        files: ['src/assets/style/*.scss'],
+        tasks: ['compass'],
         options: {
           livereload: true,
         },
-        css: {
-          files: ['src/assets/style/*.scss'],
-          tasks: ['compass'],
-        },
       },
-      browserSync: {
-        dev: {
-          bsFiles: {
-            src: [
-              'css/*.css',
-              './*.html'
-            ]
-          },
-          options: {
-            watchTask: true,
-            server: './'
-          }
-        }
-      }
-    });
-  
-    // [
-    //   'grunt-contrib-compass',
-    //   'grunt-browser-sync',
-    //   'grunt-contrib-watch'
-    // ].forEach(grunt.loadNpmTasks);
-  
-    grunt.loadNpmTasks('grunt-contrib-compass'),
-    grunt.loadNpmTasks('grunt-browser-sync'),
-    grunt.loadNpmTasks('grunt-contrib-watch');
-  
-    grunt.registerTask('default', ['browserSync', 'watch']);
-  
-  };
+    },
+  });
+
+  [
+    'grunt-contrib-compass',
+    'grunt-contrib-watch'
+  ].forEach(grunt.loadNpmTasks);
+
+  grunt.registerTask('default', ['compass']);
+};
