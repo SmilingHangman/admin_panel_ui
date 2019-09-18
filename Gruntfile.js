49b68cbdf688fbf104e1078f767b0a74747ce20e
SmilingHangman <andrejus.chomicenka@gmail.com>
2019-09-18 18:21:42 +0300

    some more job done

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,11 +18,45 @@ module.exports = function(grunt) {
         },
       },
     },
+
+    svg_sprite: {
+			generate: {
+				cwd: 'web/assets/vendor/material-design-icons',
+				src: [
+					// 'content/svg/production/ic_add_24px.svg',
+					'../../../../public/assets/img/ic_dots_24px.svg',
+					'../../../../public/assets/img/ic_logo_24px.svg',
+					'../../../../public/assets/img/ic_menu_24px.svg',
+					'../../../../public/assets/img/ic_notification_24px.svg',
+
+				],
+				dest: 'src/sprites',
+
+				options: {
+					shape: {
+						id: {
+							generator: function(filename) {
+								var id = filename.match(/ic_(\w+)_\d+/);
+								return id[1];
+							}
+						},
+					},
+					mode: {
+						symbol: {
+							dest: ''
+						}
+					}
+				}
+			}
+		}
+    
+
   });
 
   [
     'grunt-contrib-compass',
-    'grunt-contrib-watch'
+    'grunt-contrib-watch',
+    'grunt-svg-sprite'
   ].forEach(grunt.loadNpmTasks);
 
   grunt.registerTask('default', ['compass']);
